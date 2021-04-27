f3a5cc26fce4b838d5261bbe95e29f40255e3937
Septs <github@septs.pw>
2021-04-27 18:36:51 +0800

    add global flag

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { samver } from './version';
 async function main(): Promise<void> {
   const token = getInput('token');
   const registry = Boolean(getInput('registry'));
-  await npmrc.configure(token, registry);
+  const global = Boolean(getInput('global'));
+  await npmrc.configure({ token, global, registry });
 
   exportVariable('BUILD_VERSION', samver());
 }
--- src/npmrc.ts
+++ src/npmrc.ts
@@ -1,16 +1,25 @@
+import path from 'path';
+import { homedir } from 'os';
 import { update, replace } from './utils';
 
-const rc = '.npmrc';
+interface Options {
+  token: string;
+  global: boolean;
+  registry: boolean;
+}
 
-export async function configure(
-  token: string,
-  registry: boolean,
-): Promise<void> {
-  return update(rc, (lines) => {
+export async function configure(options: Options): Promise<void> {
+  const rcPath = path.join(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    options.global ? homedir() : process.env.GITHUB_WORKSPACE!,
+    '.npmrc',
+  );
+  return update(rcPath, (lines) => {
     const prefix = '//npm.pkg.github.com';
-    replace(lines, prefix, `${prefix}/:_authToken=${token}`);
-    if (registry) {
-      setRegistry(lines, process.env.GITHUB_REPOSITORY?.split('/')?.[0]);
+    replace(lines, prefix, `${prefix}/:_authToken=${options.token}`);
+    if (options.registry) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      setRegistry(lines, process.env.GITHUB_REPOSITORY!.split('/')![0]);
     }
   });
 }
